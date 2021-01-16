1f5fb1934d95413ac05a98f231efb661010a1270
张呈成 <360467435@qq.com>
2021-01-16 14:08:10 +0800

    Revert " serverSocket接收连接 -> serverSocketChannel.accept()"
    This reverts commit 33c0daa

--- transport/src/main/java/io/netty/channel/socket/nio/NioServerSocketChannel.java
+++ transport/src/main/java/io/netty/channel/socket/nio/NioServerSocketChannel.java
@@ -149,7 +149,6 @@ public class NioServerSocketChannel extends AbstractNioMessageChannel
 
     @Override
     protected int doReadMessages(List<Object> buf) throws Exception {
-        //.
         SocketChannel ch = SocketUtils.accept(javaChannel());
 
         try {
